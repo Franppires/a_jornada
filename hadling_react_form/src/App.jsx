6bd5b9b9584d50bd88e11127f06f5626b933a927
unknown <franpp22@gmail.com>
2023-02-23 12:40:18 -0300

    eighteenth commit - form

---
+++ hadling_react_form/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import BasicForm from './components/BasicForm'
+import Form from './components/Form'
+
+function App() {
+
+ 
+
+  return (
+    <>      
+      <h1>Front Beginners</h1>
+      
+      {/* <BasicForm /> */}
+      <Form />
+    </>
+  )
+}
+
+export default App
